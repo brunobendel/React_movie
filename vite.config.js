899878766e72790091c91b5ec795744b8a5c440d
Bruno <bruno.bendel9@gmai.com>
2024-03-27 10:20:23 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/react_movie",
-  proxy: {
-    '/api': {
-      target: 'https://brunobendel.github.io/react_movie',
-      changeOrigin: true,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.themoviedb.org/3',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
     },
   },
-
-})
+});
